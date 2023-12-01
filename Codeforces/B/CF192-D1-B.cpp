c9f7d86fca0ef408e69d4a7c9efe0e266fa7f72f
Soliman Elhassanein <133492248+Soliman-Elhassanein@users.noreply.github.com>
2023-12-01 11:13:51 +0200

    1 Dec 2023

---
+++ Codeforces/B/CF192-D1-B.cpp
@@ -0,0 +1,93 @@
+/*
+Link: https://codeforces.com/contest/329/problem/B
+By Soliman Elhassanein
+
+TC: O[ (r*c)^2 ]
+MC: O[ 2(r*c) ]
+
+*/
+
+
+
+// Improving input/output efficiency
+#define IOS std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define _SILENCE_CXX20_CISO646_REMOVED_WARNING
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int DFS(vector<vector<char>> map, pair<int, int> End) {
+
+	vector<vector<char>> map_e(map.size(), vector<char>(map[0].size(), 0));
+	queue<pair<int, int>> current, next;
+	int monks = 0;
+	current.push(End);
+	int counter = 0;
+	bool flag = true;
+	do
+	{
+		while (current.size()) {
+			if (map[current.front().first][current.front().second] == 'S')
+				flag = false;
+
+			if (map_e[current.front().first][current.front().second] == 0) {
+				map_e[current.front().first][current.front().second] = 1;
+
+				if (map.size() > current.front().first + 1)
+					if (map[current.front().first + 1][current.front().second] != 'T')
+						next.push({ current.front().first + 1, current.front().second });
+
+				if (-1 < current.front().first - 1)
+					if (map[current.front().first - 1][current.front().second] != 'T')
+						next.push({ current.front().first - 1, current.front().second });
+
+
+				if (map[0].size() > current.front().second + 1)
+					if (map[current.front().first][current.front().second + 1] != 'T')
+						next.push({ current.front().first, current.front().second + 1 });
+
+				if (-1 < current.front().second - 1)
+					if (map[current.front().first][current.front().second - 1] != 'T')
+						next.push({ current.front().first, current.front().second - 1 });
+			
+				if (map[current.front().first][current.front().second] != 'S' && map[current.front().first][current.front().second] != 'E')
+					monks += map[current.front().first][current.front().second] - '0';
+			
+			}
+
+			current.pop();
+		}
+		if (flag) {
+			current.swap(next);
+			counter++;
+		}
+
+	} while (flag);
+
+	return monks;
+
+}
+
+int main() {IOS;
+	
+	int r, c;
+	cin >> r >> c;
+	char temp;
+
+	vector<vector<char>> map(r, vector<char>(c, 0));
+	pair<int, int> End;
+
+	for (int i = 0; i < map.size(); i++) {
+		for (int j = 0; j < map[0].size(); j++) {
+			cin >> temp;
+			map[i][j] = temp;
+			if (temp == 'E')			
+				End = {i, j};
+		}
+	}
+
+	cout << DFS(map, End);;
+
+	return 0;
+}
