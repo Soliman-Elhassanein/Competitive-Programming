ca8e14779f1fea8d154b768833cfb26ec9de6fed
Soliman Elhassanein <133492248+Soliman-Elhassanein@users.noreply.github.com>
2023-12-02 09:03:42 +0200

    1 Dec 2023

--- Codeforces/B/CF192-D1-B Biridian Forest.cpp
+++ Codeforces/B/CF192-D1-B Biridian Forest.cpp
@@ -9,6 +9,17 @@ MC: O[ 2(r*c) ]
 
 
 
+/*
+Link: https://codeforces.com/contest/329/problem/B
+By Soliman Elhassanein
+
+TC: O[ (r*c)^2 ]
+MC: O[ 2(r*c) ]
+
+*/
+
+
+
 // Improving input/output efficiency
 #define IOS std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
 #define _SILENCE_CXX20_CISO646_REMOVED_WARNING
@@ -34,26 +45,19 @@ int BFS(vector<vector<char>> map, pair<int, int> End) {
 			if (map_e[current.front().first][current.front().second] == 0) {
 				map_e[current.front().first][current.front().second] = 1;
 
-				if (map.size() > current.front().first + 1)
-					if (map[current.front().first + 1][current.front().second] != 'T')
-						next.push({ current.front().first + 1, current.front().second });
 
-				if (-1 < current.front().first - 1)
-					if (map[current.front().first - 1][current.front().second] != 'T')
-						next.push({ current.front().first - 1, current.front().second });
+				for (int i = 0; i < 4; ++i) {
+					int x = (i == 2) - (i == 3);
+					int y = (i == 0) - (i == 1);
 
+					if (map.size() > current.front().first + x && map[0].size() > current.front().second + y && -1 < current.front().first + x && -1 < current.front().second + y)
+						if (map[current.front().first + x][current.front().second + y] != 'T')
+							next.push({ current.front().first + x, current.front().second + y});
+				}
 
-				if (map[0].size() > current.front().second + 1)
-					if (map[current.front().first][current.front().second + 1] != 'T')
-						next.push({ current.front().first, current.front().second + 1 });
-
-				if (-1 < current.front().second - 1)
-					if (map[current.front().first][current.front().second - 1] != 'T')
-						next.push({ current.front().first, current.front().second - 1 });
-			
 				if (map[current.front().first][current.front().second] != 'S' && map[current.front().first][current.front().second] != 'E')
 					monks += map[current.front().first][current.front().second] - '0';
-			
+
 			}
 
 			current.pop();
@@ -67,8 +71,9 @@ int BFS(vector<vector<char>> map, pair<int, int> End) {
 
 }
 
-int main() {IOS;
-	
+int main() {
+	IOS;
+
 	int r, c;
 	cin >> r >> c;
 	char temp;
@@ -80,8 +85,8 @@ int main() {IOS;
 		for (int j = 0; j < map[0].size(); j++) {
 			cin >> temp;
 			map[i][j] = temp;
-			if (temp == 'E')			
-				End = {i, j};
+			if (temp == 'E')
+				End = { i, j };
 		}
 	}
 
